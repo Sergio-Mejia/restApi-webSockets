cb2ac6403304cd2e87b2609be81ab509f0a6212e
Sergio Mejia <sdmejiac@gmail.com>
2024-06-17 23:02:47 -0500

    feat: details in desktop screen

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,11 +1,32 @@
 const lblPending = document.querySelector("#lbl-pending");
+const deskHeader = document.querySelector("h1");
+const noMoreAlert = document.querySelector(".alert");
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has("escritorio")) {
+  window.location = "index.html";
+  throw new Error("Escritorio es requerido");
+}
+
+const deskNumber = searchParams.get("escritorio");
+deskHeader.innerText = deskNumber;
+
+function checkTicketCount(currentCount = 0) {
+  if (currentCount === 0) {
+    noMoreAlert.classList.remove("d-none");
+  } else {
+    noMoreAlert.classList.add("d-none");
+  }
+  lblPending.innerHTML = currentCount;
+}
 
 async function loadInitialCount() {
   const pendingTickets = await fetch("/api/ticket/pending")
     .then((resp) => resp.json())
     .catch((err) => console.log(err));
 
-  lblPending.innerText = pendingTickets.length || 0;
+  checkTicketCount(pendingTickets.length);
 }
 
 function connectToWebSockets() {
@@ -15,8 +36,7 @@ function connectToWebSockets() {
     // console.log(event.data); //* on-ticket-count-change
     const { payload, type } = JSON.parse(event.data);
     if (type !== "on-ticket-count-change") return;
-    
-    lblPending.innerText = payload;
+    checkTicketCount(payload);
   };
 
   socket.onclose = (event) => {
