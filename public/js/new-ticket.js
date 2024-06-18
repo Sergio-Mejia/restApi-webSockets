f533c456290396f38757a9612478c37875bf410a
Sergio Mejia <sdmejiac@gmail.com>
2024-06-17 22:26:52 -0500

    feat: screen - new ticket

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,22 @@
+const currentTicketLbl = document.querySelector("span");
+const createTicketBtn = document.querySelector("button");
 
+async function getLastTicket() {
+  const lastTicket = await fetch("/api/ticket/last")
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err));
 
-console.log('Nuevo Ticket HTML');
+  currentTicketLbl.innerHTML = lastTicket;
+}
+
+async function createTicket() {
+  const newTicket = await fetch("/api/ticket", { method: "POST" })
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err));
+
+  currentTicketLbl.innerHTML = newTicket.number;
+}
+
+createTicketBtn.addEventListener("click", createTicket);
+
+getLastTicket();
